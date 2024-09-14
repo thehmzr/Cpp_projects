00a3119bfb92b8b4702894cecc90f09d77e9ab26
Hamza <hmza.44123@gmail.com>
2024-09-14 20:10:15 +0500

    Update patterm_Triangle.cpp

--- patterm_Triangle.cpp
+++ patterm_Triangle.cpp
@@ -5,15 +5,16 @@ using namespace std;
 
 int main()
 {
-	int height, width=0;
+	int height=0;
 	char symbol;
 	cout << "Enter Height: "; cin >> height;
 	cout << "Enter Symbol: "; cin >> symbol;
+	cout << endl;
 
 
-	for (int i = 0; i < height;i++) {
-		width++;
-		for (int j = 0; j<width; j++) {
+	for (int i = 0; i <=height;i++) {
+		
+		for (int j = 0; j<=i; j++) {
 			cout << setw(3)<<symbol;
 			
 		}
